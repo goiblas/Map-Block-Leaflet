f124285cbf15ada5d4f6039e2484e0574bc7f0a8
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2024-06-10 12:40:40 +0200

    chore: Update Leaflet lib to lastest verion

--- map-block-leaflet.php
+++ map-block-leaflet.php
@@ -7,7 +7,7 @@
  * @wordpress-plugin
  * Plugin Name: Map Block Leaflet
  * Description: Map Block Leaflet -- Allows embed maps in your contents, good alternative to Google Maps without the need for api key
- * Version:     3.1.1
+ * Version:     3.2.0
  * Author:      Jesús Olazagoitia
  * Author URI:  https://goiblas.com
  * Text Domain: map-block-leaflet
@@ -26,7 +26,7 @@ function map_block_leaflet_register() {
 	// Enqueue lib assets
 	$lib_script_path = '/lib/leaflet.js';
 	$lib_style_path = '/lib/leaflet.css';
-	$lib_version = '1.9.0';
+	$lib_version = '1.9.4';
 
 	wp_register_style( 'lib-css-map-block-leaflet', plugins_url($lib_style_path, __FILE__), array(), $lib_version );
 	wp_register_script( 'lib-js-map-block-leaflet', plugins_url($lib_script_path, __FILE__), array(), $lib_version, false );
