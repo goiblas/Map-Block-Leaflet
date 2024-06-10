58234d20a9869155581e4a734a5de0eb256a5507
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2024-06-10 12:45:06 +0200

    chore: build new version

--- build/leaflet-map-block/index.asset.php
+++ build/leaflet-map-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ee8241b67f9b390f221b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2cbf714a66e505617301');
--- build/multi-marker/index.asset.php
+++ build/multi-marker/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a158385a1dc2f78616ae');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6e65fb9cb34be81b534c');
